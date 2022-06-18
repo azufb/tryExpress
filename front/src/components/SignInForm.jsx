56cdc45257457ecfcd4cae781739499f822bd404
azufb <curiousazunas@gmail.com>
2022-06-18 20:35:47 +0900

    サインイン機能ざっくり作成

---
+++ front/src/components/SignInForm.jsx
@@ -0,0 +1,24 @@
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+
+const SignInForm = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = async (data) => {
+        await axios.post('http://localhost:3001/signIn', data).then(res => console.log('response:', res.config.data));
+    }
+
+    return (
+        <div>
+            <h2>サインインする</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>email:</label>
+                <input {...register('email')} />
+                <label>password:</label>
+                <input {...register('password')} />
+                <input type='submit' />
+            </form>
+        </div>
+    )
+};
+
+export default SignInForm;
